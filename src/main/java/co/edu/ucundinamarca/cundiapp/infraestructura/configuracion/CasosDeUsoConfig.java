8e4adbb262d8389a91d5a3765a4dcb9df9074076
crst11 <cristian02morales@gmail.com>
2026-09-22 20:56:58 -0500

    feat(cuenta): ensamblar RegistrarEstudiante como bean SCRUM-17

--- src/main/java/co/edu/ucundinamarca/cundiapp/infraestructura/configuracion/CasosDeUsoConfig.java
+++ src/main/java/co/edu/ucundinamarca/cundiapp/infraestructura/configuracion/CasosDeUsoConfig.java
@@ -1,8 +1,13 @@
 package co.edu.ucundinamarca.cundiapp.infraestructura.configuracion;
 
 import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.entrada.ListarCategoriasDeRecurso;
+import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.entrada.RegistrarEstudiante;
 import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.salida.CategoriaDeRecursoRepositorio;
+import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.salida.CifradorDeContrasenaPort;
+import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.salida.EstudianteRepositorio;
+import co.edu.ucundinamarca.cundiapp.aplicacion.puerto.salida.RelojPort;
 import co.edu.ucundinamarca.cundiapp.aplicacion.servicio.ListarCategoriasDeRecursoServicio;
+import co.edu.ucundinamarca.cundiapp.aplicacion.servicio.RegistrarEstudianteServicio;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +18,11 @@ class CasosDeUsoConfig {
 	@Bean
 	ListarCategoriasDeRecurso listarCategoriasDeRecurso(CategoriaDeRecursoRepositorio repositorio) {
 		return new ListarCategoriasDeRecursoServicio(repositorio);
+	}
+
+	@Bean
+	RegistrarEstudiante registrarEstudiante(
+			EstudianteRepositorio repositorio, CifradorDeContrasenaPort cifrador, RelojPort reloj) {
+		return new RegistrarEstudianteServicio(repositorio, cifrador, reloj);
 	}
 }
