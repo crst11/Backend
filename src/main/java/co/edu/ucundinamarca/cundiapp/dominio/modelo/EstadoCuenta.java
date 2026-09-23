c944eeb151384840c378c4700ec36839ac1cea80
crst11 <cristian02morales@gmail.com>
2026-09-22 20:56:37 -0500

    feat(cuenta): modelar la cuenta del estudiante y el correo institucional SCRUM-17

---
+++ src/main/java/co/edu/ucundinamarca/cundiapp/dominio/modelo/EstadoCuenta.java
@@ -0,0 +1,8 @@
+package co.edu.ucundinamarca.cundiapp.dominio.modelo;
+
+/** Refleja los valores permitidos por la restricción ck_estudiante_estado del esquema. */
+public enum EstadoCuenta {
+	PENDIENTE,
+	ACTIVA,
+	INACTIVA
+}
